5cacda7f9777090d42dbef11391b83c154689456
Quang Nguyen <quang.nv212@gmail.com>
2023-07-16 17:51:18 +0700

    Add react-icons lib

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Button } from "react-bootstrap";
+import { BiAtom } from "react-icons/bi";
 
 function App() {
   return (
     <div className="App">
       <Button variant="outline-success">Close me</Button>
+      <BiAtom />
     </div>
   );
 }
