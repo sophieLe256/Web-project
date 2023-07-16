3c38c295f136f28f1b089a75cc7ac515397030ef
Sophie Le <dinokuro256@gmail.com>
2023-07-16 20:01:28 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import "./App.css";
 import { Button } from "react-bootstrap";
 import { BiAtom } from "react-icons/bi";
+import Navbar from "./component/Navbar";
+import { Login } from "./Login";
+import { Register } from "./Register";
+import { useState } from "react";
 
 function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+
   return (
     <div className="App">
-      <Button variant="outline-success">Close me</Button>
-      <BiAtom />
+      {
+        currentForm === "login" ? <Login /> : Register
+      /* <Button variant="outline-success">Close me</Button>
+      <BiAtom /> */
+}
+      {/* <>
+        <Navbar/>
+      </> */}
     </div>
   );
 }
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,45 +1,51 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from 'react-bootstrap/Button';
+import { BiLogIn } from "react-icons/bi";
+import { BsCart4} from "react-icons/bs";
 
-const Navbar = () => {
-    return (
-        <div>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Disabled</a>
-                </li>
-                </ul>
-                <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>
-            </div>
-            </nav>
-        </div>
-    )
+function BasicExample() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Products</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">About Us</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Something
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+            {/* <div className="buttons">
+                <a href="" className="btn btn-outline-dark">
+                    <BiLogIn />
+                    Login
+                </a>
+                <a href="" className="btn btn-outline-dark ms-2">
+                    Register
+                </a>
+                <a href="" className="btn btn-outline-dark ms-2">
+                    <BsCart4/>
+                    Cart (0)
+                </a>
+            </div> */}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
-export default Navbar;
+export default BasicExample;
